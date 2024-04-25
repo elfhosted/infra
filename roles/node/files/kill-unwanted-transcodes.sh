d8ef92242f5fd12aea858bc0e1ad98b38ca64b46
David Young <davidy@funkypenguin.co.nz>
2024-04-25 23:42:49 +1200

    fix addon
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- roles/node/files/kill-unwanted-transcodes.sh
+++ roles/node/files/kill-unwanted-transcodes.sh
@@ -4,10 +4,10 @@
 ps aux | grep Plex | grep "hevc" | grep "libx264" |grep -v grep|awk '{print $2}' | xargs --no-run-if-empty kill -9
 
 # indicates hevc being transcoded to x264 using gpu (inefficient, but permitted for now based on user feedback)
-#ps aux | grep Plex | grep "hevc" | grep "h264_vaapi" |grep -v grep|awk '{print $2}' | xargs --no-run-if-empty kill -9
+ps aux | grep Plex | grep "hevc" | grep "h264_vaapi" |grep -v grep|awk '{print $2}' | xargs --no-run-if-empty kill -9
 
 # kill ANY hevc (almost all 4k) transcodes excluding audio / remuxing only (inefficient, but permitted for now based on user feedback)
-# ps aux | grep Plex | grep "codec" | grep "hevc" | grep -vE "grep|eae|dca|copy" |awk '{print $2}' | xargs --no-run-if-empty kill -9
+ps aux | grep Plex | grep "codec" | grep "hevc" | grep -vE "grep|eae|dca|copy" |awk '{print $2}' | xargs --no-run-if-empty kill -9
 
 # kill ANY video transcodes not using the GPU (i.e., non-plexpass users), excluding audio / remuxing 
 ps aux | grep Plex | grep -- -codec | grep -v vaapi | grep -vE "grep|eae|dca|copy" | awk '{print $2}' | xargs --no-run-if-empty kill -9
