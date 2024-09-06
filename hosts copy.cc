abda2f7dd269c477e6d15e354d817f70736eec5a
David Young <davidy@funkypenguin.co.nz>
2024-09-06 22:43:37 +1200

    WIP
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- hosts copy.cc
+++ hosts copy.cc
@@ -10,11 +10,11 @@ controllers
 public_hosts
 
 [k3s_servers]
-us-fairy01   ansible_host=us-fairy01.elfhosted.com cluster_ip=23.147.152.166 cluster_nic=bond0 interfaces=["enp1s0f0","enp1s0f1"] addresses=[23.147.152.166/29] gateway4=23.147.152.161 keepalived_priority=130
+us-fairy01   ansible_host=us-fairy01.elfhosted.com cluster_ip=10.0.44.11 cluster_nic=bond0 interfaces=["enp1s0f0","enp1s0f1"] addresses=[23.147.152.166/29,10.0.44.11/24] gateway4=23.147.152.161 keepalived_priority=130
 
 [k3s_agents]
-us-ranger01  ansible_host=us-ranger01.elfhosted.com cluster_ip=23.147.152.162 cluster_nic=bond0 interfaces=["enp1s0f0","enp1s0f1"] addresses=[23.147.152.162/29] gateway4=23.147.152.161
-us-hobbit01  ansible_host=us-hobbit01.elfhosted.com cluster_ip=23.147.152.164 cluster_nic=bond0 interfaces=["enp1s0f0","enp1s0f1"] addresses=[23.147.152.164/29] gateway4=23.147.152.161
+us-ranger01  ansible_host=us-ranger01.elfhosted.com cluster_ip=10.0.44.111 cluster_nic=bond0 interfaces=["enp1s0f0","enp1s0f1"] addresses=[23.147.152.162/29,10.0.44.111/24] gateway4=23.147.152.161
+us-hobbit01  ansible_host=us-hobbit01.elfhosted.com cluster_ip=10.0.44.151 cluster_nic=bond0 interfaces=["enp1s0f0","enp1s0f1"] addresses=[23.147.152.164/29,10.0.44.151/24] gateway4=23.147.152.161
 us-elf01     ansible_host=10.0.44.21 cluster_ip=10.0.44.21 interfaces=["enp1s0f0","enp1s0f1"] addresses=[x.x.x.x,2606:a8c0:0:b::12/64,2606:a8c0:4:1c::2/64] addresses_internal=[10.0.44.21/24,fd00:10:0:44:0::21/118] gateway4=23.147.152.161 gateway6=2606:a8c0:0:b::1
 
 # We use these for doing ansible installs
