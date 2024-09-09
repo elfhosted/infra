fe0c400ac80276233eeebf8499021742a8246de9
David Young <davidy@funkypenguin.co.nz>
2024-09-09 18:19:50 +1200

    update speedtest servers
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- hosts.cc
+++ hosts.cc
@@ -10,35 +10,32 @@ controllers
 public_hosts
 
 [k3s_servers]
-us-fairy01   ansible_host=us-fairy01.elfhosted.com cluster_ip=192.168.44.11 external_ip=23.147.152.166 cluster_nic=bond0 interfaces=["enp1s0f0","enp1s0f1"] addresses=[23.147.152.166/29,192.168.44.11/24] gateway4=23.147.152.161 keepalived_priority=130
+eagle01   ansible_host=eagle01.elfhosted.cc cluster_ip=192.168.44.11 external_ip=23.147.152.166 cluster_nic=bond0 interfaces=["enp1s0f0","enp1s0f1"] addresses=[23.147.152.166/29,192.168.44.11/24] gateway4=23.147.152.161 keepalived_priority=130
 
 [k3s_agents]
-us-ranger01  ansible_host=us-ranger01.elfhosted.com cluster_ip=192.168.44.111 external_ip=23.147.152.162 cluster_nic=bond0 interfaces=["enp1s0f0","enp1s0f1"] addresses=[23.147.152.162/29,192.168.44.111/24] gateway4=23.147.152.161
-us-hobbit01  ansible_host=us-hobbit01.elfhosted.com cluster_ip=192.168.44.151 external_ip=23.147.152.164 cluster_nic=bond0 interfaces=["enp1s0f0","enp1s0f1"] addresses=[23.147.152.164/29,192.168.44.151/24] gateway4=23.147.152.161
-us-elf01     ansible_host=192.168.44.21 cluster_ip=192.168.44.21 interfaces=["enp1s0f0","enp1s0f1"] addresses=[x.x.x.x,2606:a8c0:0:b::12/64,2606:a8c0:4:1c::2/64] addresses_internal=[192.168.44.21/24,fd00:10:0:44:0::21/118] gateway4=23.147.152.161 gateway6=2606:a8c0:0:b::1
+yankee01  ansible_host=yankee01.elfhosted.cc cluster_ip=192.168.44.21 external_ip=23.147.152.164 cluster_nic=bond0 interfaces=["enp1s0f0","enp1s0f1"] addresses=[23.147.152.164/29,192.168.44.21/24] gateway4=23.147.152.161
+yankee02  ansible_host=yankee02.elfhosted.cc cluster_ip=192.168.44.22 external_ip=23.147.152.162 cluster_nic=bond0 interfaces=["enp1s0f0","enp1s0f1"] addresses=[23.147.152.162/29,192.168.44.22/24] gateway4=23.147.152.161
 
 # We use these for doing ansible installs
 # There's no DNS here but these are saved entries in my .ssh/config
 [public_hosts]
-us-ranger01-public.elfhosted.com
-us-hobbit01-public.elfhosted.com
-us-fairy01-public.elfhosted.com
-us-elf01-public.elfhosted.com
+yankee01-public.elfhosted.cc
+yankee02-public.elfhosted.cc
+eagle01-public.elfhosted.cc
 
 [k3s_elves]
-us-elf01
+
 
 [k3s_rangers]
-us-ranger01
+yankee01
+yankee02
 
 [k3s_hobbits]
-us-hobbit01
 
 [controllers]
-us-fairy01
+eagle01
 
 [k3s_observability]
-us-hobbit01
-us-ranger01 
+eagle01
 
 [k3s_goblins]
