0f3b011d1f0ea6e26d515c2bceee3cbfa301417c
David Young <davidy@funkypenguin.co.nz>
2025-01-15 09:12:04 +1300

    remove unused csi-nfs

--- roles/node/files/prevent-syslog-death-by-nfs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# no longer required
-true
-
-# A very hacky workaround to https://github.com/cilium/cilium/issues/33395
-# systemctl stop systemd-journald.service
-# cat /dev/null > /var/log/syslog
-# cat /dev/null > /var/log/kern.log
