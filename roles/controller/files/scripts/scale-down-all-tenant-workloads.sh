7e1819bf848a99a496ae8ce7c73b1f57035a2268
David Young <davidy@funkypenguin.co.nz>
2025-06-17 12:57:03 +1200

    switch topolvm to latest
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- roles/controller/files/scripts/scale-down-all-tenant-workloads.sh
+++ roles/controller/files/scripts/scale-down-all-tenant-workloads.sh
@@ -1,14 +1,22 @@
-NAMESPACES=$(kubectl get ns | grep aa | cut -f1 -d" ")
-for NS in ${NAMESPACES}
-do
-	DEPLOYMENTS=$(kubectl get deployment -n ${NS} | cut -f1 -d" ")
-	for DEPLOYMENT in ${DEPLOYMENTS}
-	do
-		kubectl scale deployment -n ${NS} ${DEPLOYMENT} --replicas=0
-	done
-	STATEFULSETS=$(kubectl get statefulset -n ${NS} | cut -f1 -d" ")
-	for STATEFULSET in ${STATEFULSETS}
-	do
-		kubectl scale statefulset -n ${NS} ${STATEFULSET} --replicas=0
-	done
+#!/bin/bash
+set -euo pipefail
+
+NAMESPACES=$(kubectl get ns -o jsonpath='{.items[*].metadata.name}' | tr ' ' '\n' | grep '^aa-')
+
+for NS in ${NAMESPACES}; do
+    echo "Scaling down workloads in namespace: $NS"
+
+    # Scale Deployments to 0
+    DEPLOYMENTS=$(kubectl get deployments -n "$NS" -o jsonpath='{.items[*].metadata.name}')
+    for DEPLOYMENT in $DEPLOYMENTS; do
+        echo "  Scaling deployment/$DEPLOYMENT to 0 replicas"
+        kubectl scale deployment "$DEPLOYMENT" -n "$NS" --replicas=0
+    done
+
+    # Scale StatefulSets to 0
+    STATEFULSETS=$(kubectl get statefulsets -n "$NS" -o jsonpath='{.items[*].metadata.name}')
+    for STATEFULSET in $STATEFULSETS; do
+        echo "  Scaling statefulset/$STATEFULSET to 0 replicas"
+        kubectl scale statefulset "$STATEFULSET" -n "$NS" --replicas=0
+    done
 done
